a35108f6838d4b9cb174d043e5950021aded3fc4
Yori <30553063+Hubertyori@users.noreply.github.com>
2018-03-16 22:35:52 +0800

    Update MainActivity.java

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,6 +15,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +28,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClick(View view) {
+        String str = "the function is not designed";
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
+    }
+
+    public void btnRefresh(View view) {
         new DownloadUpdate().execute();
+        myUploadData();
+    }
+
+    private void myUploadData() {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH)+1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        String mWay = String.valueOf(c.get(Calendar.DAY_OF_WEEK));
+        if("1".equals(mWay)){
+            mWay ="SUNDAY";
+        }else if("2".equals(mWay)){
+            mWay ="MONDAY";
+        }else if("3".equals(mWay)){
+            mWay ="TUESDAY";
+        }else if("4".equals(mWay)){
+            mWay ="WEDNESDAY";
+        }else if("5".equals(mWay)){
+            mWay ="THURSDAY";
+        }else if("6".equals(mWay)){
+            mWay ="FRIDAY";
+        }else if("7".equals(mWay)){
+            mWay ="SATURDAY";
+        }
+        String date = null;
+        if(day>9&&month>9) {
+             date = Integer.toString(month) + '/' + Integer.toString(day) + '/' + Integer.toString(year);
+        }
+        else if(day<10&&month>9){
+             date = Integer.toString(month) + '/' + '0'+Integer.toString(day) + '/' + Integer.toString(year);
+        }
+        else if(month<10&&day>9){
+             date ='0' + Integer.toString(month) + '/' +Integer.toString(day) + '/' + Integer.toString(year);
+        }
+        else{
+             date ='0' + Integer.toString(month) + '/' +'0'+Integer.toString(day) + '/' + Integer.toString(year);
+        }
+        ((TextView) findViewById(R.id.head_date)).setText(mWay);
+        ((TextView) findViewById(R.id.tv_date)).setText(date);
+
     }
 
 
@@ -81,9 +130,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        /*
+        the changes happened while refresh the weather information
+        change the bgcolor
+        change the date
+        change the image
+        change the temperature
+         */
         protected void onPostExecute(String temperature) {
             //Update the temperature displayed
             ((TextView) findViewById(R.id.temperature_of_the_day)).setText(temperature);
+
         }
+
+
     }
 }
