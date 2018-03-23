a628935c0e5c3ca74bd708b705e6729e3c008579
Yori <30553063+Hubertyori@users.noreply.github.com>
2018-03-23 23:09:28 +0800

    Update MainActivity.java

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,13 +25,17 @@ import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] ADate = new String[]{"MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"};
+    Pattern p = Pattern.compile("location.{3}([\u4E00-\u9FA5]+).*?cond_txt_d.{3}([\u4E00-\u9FA5]+).*?date.{3}(.{10}).*?tmp_max.{3}(\\d+).*" +
+            "?cond_txt_d.{3}([\u4E00-\u9FA5]+).*?date.{3}(.{10}).*?tmp_max.{3}(\\d+).*" +
+            "?cond_txt_d.{3}([\u4E00-\u9FA5]+).*?date.{3}(.{10}).*?tmp_max.{3}(\\d+).*");
+    Calendar c = Calendar.getInstance();
+    String mWay = String.valueOf(c.get(Calendar.DAY_OF_WEEK));
+    Integer mwee = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void btnClick(View view) {
@@ -53,48 +58,52 @@ public class MainActivity extends AppCompatActivity {
             success = true;
         }
         if (success) {
-            new DownloadUpdate().execute();
             myUploadData();
+            new DownloadUpdate().execute();
         } else {
             Toast.makeText(MainActivity.this, "Please check your net-connection", Toast.LENGTH_LONG).show();
-
         }
     }
 
     private void myUploadData() {
-        Calendar c = Calendar.getInstance();
+
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH) + 1;
         int day = c.get(Calendar.DAY_OF_MONTH);
-
-        String mWay = String.valueOf(c.get(Calendar.DAY_OF_WEEK));
         if ("1".equals(mWay)) {
             mWay = "SUNDAY";
+            mwee = 6;
         } else if ("2".equals(mWay)) {
             mWay = "MONDAY";
+            mwee = 0;
         } else if ("3".equals(mWay)) {
             mWay = "TUESDAY";
+            mwee = 1;
         } else if ("4".equals(mWay)) {
             mWay = "WEDNESDAY";
+            mwee = 2;
         } else if ("5".equals(mWay)) {
             mWay = "THURSDAY";
+            mwee = 3;
         } else if ("6".equals(mWay)) {
             mWay = "FRIDAY";
+            mwee = 4;
         } else if ("7".equals(mWay)) {
             mWay = "SATURDAY";
+            mwee = 5;
         }
-        String date = null;
-        if (day > 9 && month > 9) {
-            date = Integer.toString(month) + '/' + Integer.toString(day) + '/' + Integer.toString(year);
-        } else if (day < 10 && month > 9) {
-            date = Integer.toString(month) + '/' + '0' + Integer.toString(day) + '/' + Integer.toString(year);
-        } else if (month < 10 && day > 9) {
-            date = '0' + Integer.toString(month) + '/' + Integer.toString(day) + '/' + Integer.toString(year);
-        } else {
-            date = '0' + Integer.toString(month) + '/' + '0' + Integer.toString(day) + '/' + Integer.toString(year);
-        }
+//        String date = null;
+//        if (day > 9 && month > 9) {
+//            date = Integer.toString(month) + '/' + Integer.toString(day) + '/' + Integer.toString(year);
+//        } else if (day < 10 && month > 9) {
+//            date = Integer.toString(month) + '/' + '0' + Integer.toString(day) + '/' + Integer.toString(year);
+//        } else if (month < 10 && day > 9) {
+//            date = '0' + Integer.toString(month) + '/' + Integer.toString(day) + '/' + Integer.toString(year);
+//        } else {
+//            date = '0' + Integer.toString(month) + '/' + '0' + Integer.toString(day) + '/' + Integer.toString(year);
+//        }
         ((TextView) findViewById(R.id.head_date)).setText(mWay);
-        ((TextView) findViewById(R.id.tv_date)).setText(date);
+//        ((TextView) findViewById(R.id.tv_date)).setText(date);
 
     }
 
@@ -104,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-            String stringUrl = "https://free-api.heweather.com/s6/weather/now?";
+            String stringUrl = "https://free-api.heweather.com/s6/weather/forecast?";
             HttpURLConnection urlConnection = null;
             BufferedReader reader;
             String location = "location=chongqing";
@@ -163,21 +172,91 @@ public class MainActivity extends AppCompatActivity {
          */
         protected void onPostExecute(String temperature) {
             //Update the temperature displayed
-            Pattern p = Pattern.compile("location.{3}([\u4E00-\u9FA5]+).*tmp.{3}(\\d+)\"");
             Matcher m = p.matcher(temperature);
             m = p.matcher(temperature);
             String location = null;
             String temprature = null;
+            String weather = null;
+            String date = null;
             if (m.find()) {
                 location = m.group(1);
-                temprature = m.group(2);
+                temprature = m.group(4);
+                weather = m.group(2);
+                date = m.group(3);
+
             }
-            ((TextView) findViewById(R.id.temperature_of_the_day)).setText(temprature);
-            ((TextView) findViewById(R.id.tv_location)).setText(location);
+            ImageView wea = findViewById(R.id.img_weather_condition);
+            TextView text = findViewById(R.id.temperature_of_the_day);//
+            myChangeWeather(wea,weather);//change the main image of weather
+            myChangeText(text,temprature);//change the temperature of today
+            text = findViewById(R.id.tv_location);
+            myChangeText(text,location);
+            text = findViewById(R.id.tv_date);
+            myChangeText(text,date);
 
+            //Change the list below
+            myChangeList(m);
         }
+    }
 
+    private void myChangeList(Matcher m) {
+        String temp = m.group(2);
+        ImageView wea = findViewById(R.id.day_one);
+        myChangeWeather(wea,temp);
 
+        temp = ADate[mwee].substring(0,3);
+        TextView text = findViewById(R.id.text_day_one);
+        myChangeText(text,temp);
+
+        wea = findViewById(R.id.day_two);
+        temp = m.group(5);
+        myChangeWeather(wea,temp);
+
+        temp = ADate[(mwee+1)%7].substring(0,3);
+        text = findViewById(R.id.text_day_tow);
+        myChangeText(text,temp);
+
+        temp = m.group(8);
+        wea = findViewById(R.id.day_thr);
+        myChangeWeather(wea,temp);
+
+        temp = ADate[(mwee+2)%7].substring(0,3);
+        text = findViewById(R.id.text_day_thr);
+        myChangeText(text,temp);
+
+        temp = ADate[(mwee+3)%7].substring(0,3);
+        text = findViewById(R.id.text_day_fou);
+        myChangeText(text,temp);
+    }
+
+    private void myChangeText(TextView viewById, String location) {
+        viewById.setText(location);
+    }
+
+    private void myChangeWeather(ImageView wea, String weather) {
+        switch (weather){
+            case "阴":
+                wea.setImageResource(R.drawable.windy_small);
+                break;
+            case"小雨":
+                wea.setImageResource(R.drawable.rainy_small);
+                break;
+            case "晴":
+                wea.setImageResource(R.drawable.sunny_small);
+                break;
+            case"阵雨":
+                wea.setImageResource(R.drawable.rainy_small);
+                break;
+            case "多云":
+                wea.setImageResource(R.drawable.partly_sunny_small);
+                break;
+            case "晴间多云":
+                wea.setImageResource(R.drawable.partly_sunny_small);
+                break;
+            default:
+
+                break;
+        }
     }
 
 }
